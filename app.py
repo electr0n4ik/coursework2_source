52aa9f5435b7e5cc5a6ff96f65ae3ff180fb4c1b
elecr0n4ik <andreyshka3@gmail.com>
2023-01-13 10:20:02 +0300

    Step 6 start

--- app.py
+++ app.py
@@ -23,16 +23,19 @@ def search_page():
     """
     Поиск и вывод постов при обращении на GET /search/?s=...
     """
-    list_find_posts = []
     s = request.values.get("s") # можно делать поиск через форму "найти" и через адресную строку
 
     if len(list(s)) == 0 or s.isdigit(): # обработка пустого или с цифрой запроса
         return render_template("search_empty.html")
+
     else:
-        # logging.info(f"Выполнен поиск по запросу {s}") # логирование при выполнении поиска
-
-        return render_template("search.html", s=s, posts=search_for_posts(s), amount_posts=len(search_for_posts(s)))
-
+        s_list = s.split()
+        for symbol in s_list:
+            if symbol.isalpha():
+                # logging.info(f"Выполнен поиск по запросу {s}") # логирование при выполнении поиска
+                return render_template("search.html", s=s, posts=search_for_posts(s), amount_posts=len(search_for_posts(s)))
+            else:
+                return render_template("search_empty.html")
 
 @app.route("/users/<username>")
 def user_page(username):
@@ -40,6 +43,17 @@ def user_page(username):
     Вывод постов конкретного пользователя GET /users/<username>
     """
     return render_template("user_feed.html", posts=get_posts_by_user(username))
+
+
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
--- utils_test/step_5_conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import run
-from app import *
-
-# создаем фикстуру для тестирования всех вьюшек
-@pytest.fixture()
-def test_client():
-    app = run.app
-    return app.test_client()
