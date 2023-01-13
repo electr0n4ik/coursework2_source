229da094a753ff6e92a7e847f494af51739ede13
elecr0n4ik <andreyshka3@gmail.com>
2023-01-13 11:08:04 +0300

    Step 7 start

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, jsonify
 from utils import *
 
 # Импортируем блюпринты из их пакетов
@@ -14,14 +14,15 @@ app.register_blueprint(main_blueprint)
 # Регистрируем блюпринт с обработкой запроса при обращении к GET /post
 app.register_blueprint(post_show_blueprint)
 
+
 @app.route("/search", methods=["GET", "POST"])
 def search_page():
     """
     Поиск и вывод постов при обращении на GET /search/?s=...
     """
-    s = request.values.get("s") # можно делать поиск через форму "найти" и через адресную строку
+    s = request.values.get("s")  # можно делать поиск через форму "найти" и через адресную строку
 
-    if len(list(s)) == 0 or s.isdigit(): # обработка пустого или с цифрой запроса
+    if len(list(s)) == 0 or s.isdigit():  # обработка пустого или с цифрой запроса
         return render_template("search_empty.html")
 
     else:
@@ -29,9 +30,11 @@ def search_page():
         for symbol in s_list:
             if symbol.isalpha():
                 # logging.info(f"Выполнен поиск по запросу {s}") # логирование при выполнении поиска
-                return render_template("search.html", s=s, posts=search_for_posts(s), amount_posts=len(search_for_posts(s)))
+                return render_template("search.html", s=s, posts=search_for_posts(s),
+                                       amount_posts=len(search_for_posts(s)))
             else:
                 return render_template("search_empty.html")
+
 
 @app.route("/users/<username>")
 def user_page(username):
@@ -51,5 +54,15 @@ def internal_server_error(error):
     return render_template("500.html"), 500
 
 
+@app.route("/api/posts")
+def posts_in_json():
+    return jsonify(get_posts_all())
+
+
+@app.route("/api/posts/<post_id>")
+def one_post_in_json(post_id):
+    return jsonify(get_post_by_pk(post_id))
+
+
 if __name__ == "__main__":
-	app.run()
+    app.run()
